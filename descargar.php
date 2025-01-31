a72a37e69e624acd8bb956e120748a8e0d9e6f5a
NeonFoxCipher <hoffmannlucas37@gmail.com>
2025-01-31 13:26:53 -0300

    descargar.php

--- descargar.php
+++ descargar.php
@@ -2,13 +2,16 @@
 $file = 'log.txt';
 
 if (file_exists($file)) {
+    // Forzar la descarga del archivo
     header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Content-Type: text/plain'); // Tipo MIME adecuado
+    header('Content-Disposition: attachment; filename="log.txt"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
+
+    // Leer y enviar el contenido del archivo
     readfile($file);
     exit;
 } else {
